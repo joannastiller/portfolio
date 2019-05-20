a04be6e7559d3b0e9d537a737b25160a6cd23eb9
Joanna Stiller <joannastiller@yahoo.com>
2019-05-20 19:42:36 +0200

    Add server side form validation

--- script.js
+++ script.js
@@ -2,222 +2,246 @@
 
 const burger = document.querySelector("i.fa-bars");
 const times = document.querySelector("i.fa-times");
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
 
-burger.addEventListener('click', function () {
-    burger.classList.remove('active');
-    times.classList.add('active');
-    nav.classList.add('active');
-})
+burger.addEventListener("click", function() {
+  burger.classList.remove("active");
+  times.classList.add("active");
+  nav.classList.add("active");
+});
 
-times.addEventListener('click', function () {
-    times.classList.remove('active');
-    burger.classList.add('active');
-    nav.classList.remove('active');
-})
+times.addEventListener("click", function() {
+  times.classList.remove("active");
+  burger.classList.add("active");
+  nav.classList.remove("active");
+});
 
-$('nav ul li').on('click', function () {
-    $('body, html').animate({
-        scrollTop: $("#" + $(this).attr('class')).offset().top
-    }, 1000)
+$("nav ul li").on("click", function() {
+  $("body, html").animate(
+    {
+      scrollTop: $("#" + $(this).attr("class")).offset().top
+    },
+    1000
+  );
 
-    $('i.fa-times').removeClass('active');
-    $('i.fa-bars').addClass('active');
-    $('nav').removeClass('active');
-})
+  $("i.fa-times").removeClass("active");
+  $("i.fa-bars").addClass("active");
+  $("nav").removeClass("active");
+});
 
 // SHOW MORE BUTTON
 
-$(function () {
-    $("div.project").slice(0, 4).show();
-    $("#loadMore").on('click', function (e) {
-        e.preventDefault();
-        $("div.project:hidden").slice(0, 4).slideDown();
-        if ($("div.project:hidden").length == 0) {
-            $("#loadMore").fadeOut('slow');
-        }
-        $('html,body').animate({
-            scrollTop: $(this).offset().top - 200
-        }, 1500);
-    });
+$(function() {
+  $("div.project")
+    .slice(0, 4)
+    .show();
+  $("#loadMore").on("click", function(e) {
+    e.preventDefault();
+    $("div.project:hidden")
+      .slice(0, 4)
+      .slideDown();
+    if ($("div.project:hidden").length == 0) {
+      $("#loadMore").fadeOut("slow");
+    }
+    $("html,body").animate(
+      {
+        scrollTop: $(this).offset().top - 200
+      },
+      1500
+    );
+  });
 });
 
 //BACK TO TOP
 
-$('.totop').click(function () {
-    $('body,html').animate({
-        scrollTop: 0
-    }, 600);
-    return false;
+$(".totop").click(function() {
+  $("body,html").animate(
+    {
+      scrollTop: 0
+    },
+    600
+  );
+  return false;
 });
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-        $('.totop').fadeIn();
-    } else {
-        $('.totop').fadeOut();
-    }
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 50) {
+    $(".totop").fadeIn();
+  } else {
+    $(".totop").fadeOut();
+  }
 });
 
 //STACK ANIMATION
 
-var $animation_elements = $('.animation-element');
+var $animation_elements = $(".animation-element");
 var $window = $(window);
 
 function check_if_in_view() {
-    var window_height = $window.height();
-    var window_top_position = $window.scrollTop();
-    var window_bottom_position = (window_top_position + window_height);
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = window_top_position + window_height;
 
-    $.each($animation_elements, function () {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = element_top_position + element_height;
 
-        //check to see if this current container is within viewport
-        if ((element_bottom_position >= window_top_position - 100) &&
-            (element_top_position <= window_bottom_position)) {
-            $element.addClass('in-view');
-        } else {
-            $element.removeClass('in-view');
-        }
-    });
+    //check to see if this current container is within viewport
+    if (
+      element_bottom_position >= window_top_position - 100 &&
+      element_top_position <= window_bottom_position
+    ) {
+      $element.addClass("in-view");
+    } else {
+      $element.removeClass("in-view");
+    }
+  });
 }
 
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
-
+$window.on("scroll resize", check_if_in_view);
+$window.trigger("scroll");
 
 // INPUT VALIDATION
 
 // Add the novalidate attribute when the JS loads
-var forms = document.querySelectorAll('.validate');
+var forms = document.querySelectorAll(".validate");
 for (var i = 0; i < forms.length; i++) {
-    forms[i].setAttribute('novalidate', true);
+  forms[i].setAttribute("novalidate", true);
 }
 
 // Validate the field
-var hasError = function (field) {
+var hasError = function(field) {
+  // Don't validate submit field
+  if (field.type === "submit") return;
 
-    // Don't validate submit field
-    if (field.type === 'submit') return;
+  // Get validity
+  var validity = field.validity;
 
-    // Get validity
-    var validity = field.validity;
+  // If valid, return null
+  if (validity.valid) return;
 
-    // If valid, return null
-    if (validity.valid) return;
+  // If field is required and empty
+  if (validity.valueMissing) return "Wypełnij to pole";
 
-    // If field is required and empty
-    if (validity.valueMissing) return 'Wypełnij to pole';
+  // If not the right type
+  if (validity.typeMismatch) {
+    if (field.type === "email") return "Wpisz poprawny adres e-mail";
+  }
 
-    // If not the right type
-    if (validity.typeMismatch) {
-        if (field.type === 'email') return 'Wpisz poprawny adres e-mail';
-    }
+  // If too short
+  if (validity.tooShort)
+    return (
+      "Wpisana wartość jest za krótka. Musi mieć co najmniej " +
+      field.getAttribute("minLength") +
+      " znaków. W tej chwili jest " +
+      field.value.length +
+      " znaków."
+    );
 
-    // If too short
-    if (validity.tooShort) return 'Wpisana wartość jest za krótka. Musi mieć co najmniej ' + field.getAttribute('minLength') + ' znaków. W tej chwili jest ' + field.value.length + ' znaków.';
+  // If too long
+  if (validity.tooLong)
+    return (
+      "Skróć wartość do " +
+      field.getAttribute("maxLength") +
+      " znaków. W tej chwili jest " +
+      field.value.length +
+      " znaków."
+    );
 
-    // If too long
-    if (validity.tooLong) return 'Skróć wartość do ' + field.getAttribute('maxLength') + ' znaków. W tej chwili jest ' + field.value.length + ' znaków.';
+  // If pattern doesn't match
+  if (validity.patternMismatch) {
+    // If pattern info is included, return custom error
+    if (field.hasAttribute("title")) return field.getAttribute("title");
 
-    // If pattern doesn't match
-    if (validity.patternMismatch) {
-
-        // If pattern info is included, return custom error
-        if (field.hasAttribute('title')) return field.getAttribute('title');
-
-        // Otherwise, generic error
-        return 'Wpisz poprawną wartość';
-
-    }
-    // If all else fails, return a generic catchall error
-    return 'Sprawdź, czy pole jest wypełnione prawidłowo';
+    // Otherwise, generic error
+    return "Wpisz poprawną wartość";
+  }
+  // If all else fails, return a generic catchall error
+  return "Sprawdź, czy pole jest wypełnione prawidłowo";
 };
 
 //Show an error message
-var showError = function (field, error) {
+var showError = function(field, error) {
+  // Add error class to field
+  field.classList.add("error");
 
-    // Add error class to field
-    field.classList.add('error');
+  // Get field id or name
+  var id = field.id || field.name;
+  if (!id) return;
 
-    // Get field id or name
-    var id = field.id || field.name;
-    if (!id) return;
+  // Check if error message field already exists
+  // If not, create one
+  var message = field.form.querySelector(".error-message#error-for-" + id);
+  if (!message) {
+    message = document.createElement("div");
+    message.className = "error-message";
+    message.id = "error-for-" + id;
+    field.parentNode.insertBefore(message, field.nextSibling);
+  }
 
-    // Check if error message field already exists
-    // If not, create one
-    var message = field.form.querySelector('.error-message#error-for-' + id);
-    if (!message) {
-        message = document.createElement('div');
-        message.className = 'error-message';
-        message.id = 'error-for-' + id;
-        field.parentNode.insertBefore(message, field.nextSibling);
-    }
+  // Add ARIA role to the field
+  field.setAttribute("aria-describedby", "error-for-" + id);
 
-    // Add ARIA role to the field
-    field.setAttribute('aria-describedby', 'error-for-' + id);
+  // Update error message
+  message.innerHTML = error;
 
-    // Update error message
-    message.innerHTML = error;
-
-    // Show error message
-    message.style.display = 'block';
-    message.style.visibility = 'visible';
-
+  // Show error message
+  message.style.display = "block";
+  message.style.visibility = "visible";
 };
 
 // Remove the error message
-var removeError = function (field) {
+var removeError = function(field) {
+  // Remove error class to field
+  field.classList.remove("error");
 
-    // Remove error class to field
-    field.classList.remove('error');
+  // Remove ARIA role from the field
+  field.removeAttribute("aria-describedby");
 
-    // Remove ARIA role from the field
-    field.removeAttribute('aria-describedby');
+  // Get field id or name
+  var id = field.id || field.name;
+  if (!id) return;
 
-    // Get field id or name
-    var id = field.id || field.name;
-    if (!id) return;
+  // Check if an error message is in the DOM
+  var message = field.form.querySelector(".error-message#error-for-" + id + "");
+  if (!message) return;
 
-    // Check if an error message is in the DOM
-    var message = field.form.querySelector('.error-message#error-for-' + id + '');
-    if (!message) return;
-
-    // If so, hide it
-    message.innerHTML = '';
-    message.style.display = 'none';
-    message.style.visibility = 'hidden';
-
+  // If so, hide it
+  message.innerHTML = "";
+  message.style.display = "none";
+  message.style.visibility = "hidden";
 };
 
 // Listen to all blur events
-document.addEventListener('blur', function (event) {
-
+document.addEventListener(
+  "blur",
+  function(event) {
     // Only run if the field is in a form to be validated
-    if (!event.target.form.classList.contains('validate')) return;
+    if (!event.target.form.classList.contains("validate")) return;
 
     // Validate the field
     var error = hasError(event.target);
 
     // If there's an error, show it
     if (error) {
-        showError(event.target, error);
-        return;
+      showError(event.target, error);
+      return;
     }
 
     // Otherwise, remove any existing error message
     removeError(event.target);
-
-}, true);
+  },
+  true
+);
 
 // Check all fields on submit
-document.addEventListener('submit', function (event) {
-
+document.addEventListener(
+  "submit",
+  function(event) {
     // Only run on forms flagged for validation
-    if (!event.target.classList.contains('validate')) return;
+    if (!event.target.classList.contains("validate")) return;
 
     // Get all of the form elements
     var fields = event.target.elements;
@@ -226,20 +250,74 @@ document.addEventListener('submit', function (event) {
     // Store the first field with an error to a variable so we can bring it into focus later
     var error, hasErrors;
     for (var i = 0; i < fields.length; i++) {
-        error = hasError(fields[i]);
-        if (error) {
-            showError(fields[i], error);
-            if (!hasErrors) {
-                hasErrors = fields[i];
-            }
+      error = hasError(fields[i]);
+      if (error) {
+        showError(fields[i], error);
+        if (!hasErrors) {
+          hasErrors = fields[i];
         }
+      }
     }
 
     // If there are errors, don't submit form and focus on first element with error
     if (hasErrors) {
-        event.preventDefault();
-        hasErrors.focus();
+      event.preventDefault();
+      hasErrors.focus();
     }
 
     // Otherwise, let the form submit normally
-}, false);
+  },
+  false
+);
+
+// SERVER SIDE VALIDATION
+
+$(function() {
+  function after_form_submitted(data) {
+    if (data.result == "success") {
+      $("form#reused_form").hide();
+      $("#success_message").show();
+      $("#error_message").hide();
+    } else {
+      $("#error_message").append("<ul></ul>");
+
+      jQuery.each(data.errors, function(key, val) {
+        $("#error_message ul").append("<li>" + key + ":" + val + "</li>");
+      });
+      $("#success_message").hide();
+      $("#error_message").show();
+
+      //reverse the response on the button
+      $('button[type="button"]', $form).each(function() {
+        $btn = $(this);
+        label = $btn.prop("orig_label");
+        if (label) {
+          $btn.prop("type", "submit");
+          $btn.text(label);
+          $btn.prop("orig_label", "");
+        }
+      });
+    } //else
+  }
+
+  $("#reused_form").submit(function(e) {
+    e.preventDefault();
+
+    $form = $(this);
+    //show some response on the button
+    $('button[type="submit"]', $form).each(function() {
+      $btn = $(this);
+      $btn.prop("type", "button");
+      $btn.prop("orig_label", $btn.text());
+      $btn.text("Sending ...");
+    });
+
+    $.ajax({
+      type: "POST",
+      url: "handler.php",
+      data: $form.serialize(),
+      success: after_form_submitted,
+      dataType: "json"
+    });
+  });
+});
